17cefad9836b092e36d39e2e991db85dfd66a17a
volodymyr-ivaniuk <ivaniukv23@gmail.com>
2025-06-29 00:38:19 +0200

    custom hooks section

---
+++ 11-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+import {fetchUserPlaces} from "../http.js";
+
+export function useFetch(fetchFn, initValue) {
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initValue);
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsFetching(true);
+            try {
+                const data = await fetchFn();
+                setFetchedData(data);
+            } catch (error) {
+                setError({ message: error.message || 'Failed to fetch data.' });
+            }
+
+            setIsFetching(false);
+        }
+
+        fetchData();
+    }, [fetchFn]);
+
+    return {
+        isFetching, fetchedData, error, setFetchedData
+    }
+}
